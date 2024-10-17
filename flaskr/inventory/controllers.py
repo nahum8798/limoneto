f92755a6d7768b64717fa94c0324626e4c4b9f17
nahum8798 <nahum8798@gmail.com>
2024-10-16 22:18:37 -0300

    Despligue del formulario en el modal

--- flaskr/inventory/controllers.py
+++ flaskr/inventory/controllers.py
@@ -1,5 +1,13 @@
+from unicodedata import category
+
+from werkzeug.utils import redirect
+from flask import url_for
+from flaskr.extensions import db
 from flaskr.inventory import inventory_blueprint
 from flask import render_template
+from .forms import CategoryForm, SubCategoryForm
+from .models import Categories, SubCategories
+
 
 @inventory_blueprint.route('/inventory')
 def inventory():
@@ -7,8 +15,20 @@ def inventory():
     esta funcion rendiriza la pantalla de inventario y carga las categorias cargadas
     :return:
     """
-    return render_template('inventory.html')
+    form = CategoryForm()
+    return render_template('inventory.html', form=form)
 
 @inventory_blueprint.route('/add_categorie')
 def add_category():
-    pass
+
+    form = CategoryForm() # Instancia del formulario de categoria
+    if form.validate_on_submit():
+        newCategory = Categories(
+            category_name = form.category_name,
+            category_description = form.category_description
+        )
+        db.session.add(newCategory)
+        db.session.commit()
+        return redirect(url_for('inventory.inventory'))
+
+    return render_template('inventory.html')
--- flaskr/inventory/models.py
+++ flaskr/inventory/models.py
@@ -25,7 +25,7 @@ class SubCategories(db.Model):
     id_subcategory = db.Column(db.Integer, primary_key=True, nullable=False)
     subcategory_name = db.Column(db.String(100), nullable=False)
     category_description = db.Column(db.Text, nullable=True)
-    id_category = db.Column(db.Intenger, db.ForeignKey('categories.id_category'), nullable=False)
+    id_category = db.Column(db.Integer, db.ForeignKey('categories.id_category'), nullable=False)
     date_create = db.Column(db.DateTime, default=datetime.utcnow)
     date_update = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
